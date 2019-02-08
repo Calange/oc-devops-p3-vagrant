f765bd679e4551830292802b1a1b4d58e6ff5e05
Calange Favreau <calangef@gmail.com>
2019-02-08 19:15:37 +0100

    add package version variables

--- provision.sh
+++ provision.sh
@@ -2,23 +2,40 @@
 
 set -e # exit script immediately on first error
 
+# package version variables
+
+neovim_version="0.1.7-4"
+ansible_version="2.2.1.0-2"
+dockerce_version="5:18.09.1~3-0~debian-stretch"
+dockercompose_version="1.23.2"
+
+# script
+
 echo -e "\e[34m\e[1m=== Update current packages ==="
 apt-get update
-echo -e "\e[34m\e[1m=== Neovim installation ==="
-apt-get install -y neovim
-echo -e "\e[34m\e[1m=== Ansible installation ==="
-apt-get install -y ansible
-echo -e "\e[34m\e[1m=== Docker CE installation ==="
+
+echo -e "\e[34m\e[1m=== Neovim ($neovim_version) installation ==="
+apt-get install -y neovim=$neovim_version
+
+echo -e "\e[34m\e[1m=== Ansible ($ansible_version) installation ==="
+apt-get install -y ansible=$ansible_version
+
+echo -e "\e[34m\e[1m=== Docker CE ($dockerce_version) installation ==="
 apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg --output docker_gpg
 apt-key add docker_gpg && rm -f docker_gpg
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 apt-get update
-apt-get install -y docker-ce
-echo -e "\e[34m\e[1m=== Check if Neovim, Ansible and Docker CE are present ==="
+apt-get install -y docker-ce=$dockerce_version
+
+echo -e "\e[34m\e[1m=== Docker-compose ($dockercompose_version) installation ==="
+curl -L "https://github.com/docker/compose/releases/download/$dockercompose_version/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+echo -e "\e[34m\e[1m=== Check if Neovim, Ansible, Docker CE and Docker-compose are present ==="
 dpkg-query -l neovim ansible docker-ce
-echo -e "\e[34m\e[1m=== Build docker image: oc-devops-p3-docker ==="
+docker-compose --version
+
+echo -e "\e[34m\e[1m=== Docker-compose up: oc-devops-p3-docker ==="
 cd oc-devops-p3-docker
-docker build -t oc-devops-p3-docker .
-echo -e "\e[34m\e[1m=== Run docker image: oc-devops-p3-docker ==="
-docker run --name mywebsite.com -v /home/vagrant/oc-devops-p3-docker/mywebsite.com:/usr/share/nginx/html:ro -d -p 80:80 oc-devops-p3-docker
+docker-compose up -d --build
