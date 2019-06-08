61a59ace31f6fd792bd7b4f16dcff01f35eb51be
Calange Favreau <cfavreau@prismamedia.com>
2019-06-08 12:41:50 +0200

    wip

--- 01-install-docker.sh
+++ 01-install-docker.sh
@@ -27,6 +27,6 @@ echo -e "\e[34m\e[1m=== Check if Neovim, Ansible, Docker CE and Docker-compose a
 dpkg-query -l neovim ansible docker-ce
 docker-compose --version
 
-echo -e "\e[34m\e[1m=== Docker-compose up: oc-devops-p8-prestashop ==="
-cd oc-devops-p8-prestashop
-docker-compose up -d --build
+# echo -e "\e[34m\e[1m=== Docker-compose up: oc-devops-p8-prestashop ==="
+# cd oc-devops-p8-prestashop
+# docker-compose up -d --build
